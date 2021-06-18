299f4186e4e2f0e38643496f720fbcb9eefd05f7
Amy Ni <amyni@Amys-iMac-Pro.fios-router.home>
2021-06-18 12:00:24 -0400

    edited analysis

--- analysis/analyzeSessionData.m
+++ analysis/analyzeSessionData.m
@@ -71,9 +71,14 @@ temp = load(pathToFile,'data'); data = temp.data; clear temp;
 % Save session number in 'data' struct.
 data.sessionNumber = sessionNumber;
     
-%% Analyze performance on each condition separately
+%% Analyze performance on each noise level separately
 %
-% Get the identifies and number of conditions, and save.
+% Get the identifies and number of noise levels, and save.
+noiseLevels  = unique(data.imageNoiseLevel);
+nNoiseLevels = numel(noiseLevels);
+data.noiseLevels = noiseLevels;
+
+% Get the identifies and number of conditions, and save (same per noise level).
 conditions      = unique(data.imageCondition);
 nConditions     = numel(conditions);
 data.conditions = conditions;
@@ -83,74 +88,100 @@ comparisons      = unique(data.imageComparison);
 nComparisons     = numel(comparisons);
 data.comparisons = comparisons;
 
-for ii = 1:nConditions
-    % Find image index for the image with the center position for this condition.
-    centerpos = conditions(ii);
-    centerIdx = intersect(find(data.imageCondition==centerpos),find(data.imageComparison==0));
+% Analyze each noise level separately.
+for nn = 1:nNoiseLevels
     
-    % Get trials in this condition.
-    trials = any(data.trialOrder==centerIdx,2);
+    % Get trial indices for this noise level.
+    noiseL       = noiseLevels(nn);
+    trialsNoise  = data.trialNoiseLevel==noiseL;
+    nTrialsNoise = sum(trialsNoise);
+
+    % Get the image indices, target offset amount, and observer response per trial.
+    imagesN    = data.trialOrder(trialsNoise,:);
+    offsetsN   = data.trialOrderComparison(trialsNoise,:);
+    responsesN = data.selectedResponse(trialsNoise);
     
-    % Get the comparison amount and observer response per trial.
-    offsetsC = data.trialOrderComparison(trials,:);
-    comparisonsC = sum(offsetsC,2);
-    responsesC = data.selectedResponse(trials);
-    
-    % Calculate performance per comparison amount.
-    performanceC = nan(nComparisons,1);
-    for jj = 1:nComparisons
-        comparisonThis = comparisons(jj);
-        offsetsThis    = offsetsC(comparisonsC==comparisonThis,:);
-        responsesThis  = responsesC(comparisonsC==comparisonThis);
+    % Analyze performance on each condition separately.
+    for ii = 1:nConditions
+
+        % Find image index for the image with the center position for this condition.
+        centerpos = conditions(ii);
+        centerIdx = intersect(find(data.imageNoiseLevel==noiseL), ...
+                    intersect(find(data.imageCondition==centerpos),find(data.imageComparison==0)));
+
+        % Get the target offset amounts, comparison amount, and observer response per trial.
+        trialsC      = any(imagesN==centerIdx,2);
+        offsetsC     = offsetsN(trialsC,:);
+        comparisonsC = sum(offsetsC,2);
+        responsesC   = responsesN(trialsC);
         
-        % Calculate proportion observer chose the comparison as rightward.
-        if comparisonThis==0
-            choseRight = find(responsesThis==2);
-        else
-            choseRight = [intersect(find(offsetsThis(:,1)==0), find(responsesThis==2)); ...
-                          intersect(find(offsetsThis(:,2)==0), find(responsesThis==1))];
+        % Calculate performance per comparison amount.
+        performanceC = nan(nComparisons,1);
+        for jj = 1:nComparisons
+            comparisonThis = comparisons(jj);
+            offsetsThis    = offsetsC(comparisonsC==comparisonThis,:);
+            responsesThis  = responsesC(comparisonsC==comparisonThis);
+            
+            % Calculate proportion observer chose the comparison as rightward.
+            if comparisonThis==0
+                choseRight = find(responsesThis==2);
+            else
+                choseRight = [intersect(find(offsetsThis(:,1)==0), find(responsesThis==2)); ...
+                    intersect(find(offsetsThis(:,2)==0), find(responsesThis==1))];
+            end
+            performanceC(jj) = numel(choseRight)/numel(responsesThis);
         end
-        performanceC(jj) = numel(choseRight)/numel(responsesThis);
+        
+        % Save performance for this condition (in this noise level).
+        noiseLevelName = sprintf('%s%d','noiseLevel',noiseL);
+        data.performance.(noiseLevelName).performancePerCondition{ii,1} = performanceC;
     end
-    
-    % Save performance for this condition.
-    data.performancePerCondition{ii,1} = performanceC;
 end
 
-%% Plot performance on each condition separately
+%% Plot performance on each condition separately, for each noise level
 if plotFigures
-    for ii = 1:nConditions
+    for nn = 1:nNoiseLevels
+        for ii = 1:nConditions
+            figure;
+            noiseLevelName  = sprintf('%s%d','noiseLevel',noiseLevels(nn));
+            performancethis = data.performance.(noiseLevelName).performancePerCondition{ii};
+            plot(comparisons,performancethis,'ok','MarkerFace','k');
+            hold on;
+            title({sprintf('%s%s%s%d: %s%d %s%d',experimentName,subjectName,'\_', sessionNumber, ...
+                           'noiseLevel',noiseLevels(nn),'condition',ii),''});
+            xlabel(sprintf('Comparison offset rightward'));
+            ylabel('Proportion chose comparison as rightward');
+            axis([-Inf Inf 0 1]);
+            set(gca,'tickdir','out');
+            box off; hold off;
+        end
+    end
+end
+
+%% Plot performance for all conditions combined, for each noise level
+if plotFigures
+    for nn = 1:nNoiseLevels
+        noiseLevelName  = sprintf('%s%d','noiseLevel',noiseLevels(nn));
+        
+        % Average performance across all conditions.
+        performanceAll = nan(nComparisons,nConditions);
+        for ii = 1:nConditions
+            performanceAll(:,ii) = data.performance.(noiseLevelName).performancePerCondition{ii};
+        end
+        performanceAll = mean(performanceAll,2);
+        
+        % Plot average across conditions.
         figure;
-        plot(comparisons,data.performancePerCondition{ii},'ok','MarkerFace','k');
+        plot(comparisons,performanceAll,'ok','MarkerFace','k');
         hold on;
-        title({sprintf('%s%s%s%d: %s %d',experimentName,subjectName,'\_',sessionNumber,'condition',ii),''});
+        title({sprintf('%s%s%s%d: %s%d %s',experimentName,subjectName,'\_', sessionNumber, ...
+                           'noiseLevel',noiseLevels(nn),'all conditions'),''});
         xlabel(sprintf('Comparison offset rightward'));
         ylabel('Proportion chose comparison as rightward');
         axis([-Inf Inf 0 1]);
         set(gca,'tickdir','out');
         box off; hold off;
     end
-end
-
-%% Plot performance for all conditions combined
-if plotFigures
-    % Average performance across all conditions.
-    performanceAll = nan(nComparisons,nConditions);
-    for ii = 1:nConditions
-        performanceAll(:,ii) = data.performancePerCondition{ii};
-    end
-    performanceAll = mean(performanceAll,2);
-    
-    % Plot average across conditions.
-    figure;
-    plot(comparisons,performanceAll,'ok','MarkerFace','k');
-    hold on;
-    title({sprintf('%s%s%s%d: %s',experimentName,subjectName,'\_',sessionNumber,'all conditions'),''});
-    xlabel(sprintf('Comparison offset rightward'));
-    ylabel('Proportion chose comparison as rightward');
-    axis([-Inf Inf 0 1]);
-    set(gca,'tickdir','out');
-    box off; hold off;
 end
 
 %% Save data analysis results
