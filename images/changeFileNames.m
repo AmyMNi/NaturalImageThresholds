80fb972d600f6d9001d1eb47f7444cfac7b47153
Amy Ni <amyni@Amys-iMac-Pro.fios-router.home>
2021-06-26 01:56:14 -0400

    minor edits

--- images/changeFileNames.m
+++ images/changeFileNames.m
@@ -5,8 +5,8 @@ function changeFileNames(varargin)
 %   changeFileNames()
 %
 % Description:
-%   Change the name of each file in the specified folder/s, and save the
-%   file with the revised name in a new folder/s.
+%   Change the name of each file in the specified folder, and save the
+%   file with the revised name in a new folder.
 %
 % Optional parameter/value:
 %   'experimentName' : (string) Name of experiment folder (default: 'Experiment000')
@@ -26,30 +26,21 @@ experimentName = parser.Results.experimentName;
 % Specify project name.
 projectName = 'NaturalImageThresholds';
 
-% Set path to input folder #1.
-pathToFolder1 = fullfile(getpref(projectName,'BaseDir'),experimentName,'ImageScenes');
+% Set path to input folder.
+pathToFolder = fullfile(getpref(projectName,'BaseDir'),experimentName,'ImageScenes');
 
-% Set path to output folder #1.
-pathToOutput1 = fullfile(getpref(projectName,'BaseDir'),experimentName,'ImageScenesRENAMED');
-if ~exist(pathToOutput1, 'dir')
-    mkdir(pathToOutput1);
+% Set path to output folder.
+pathToOutput = fullfile(getpref(projectName,'BaseDir'),experimentName,'ImageScenesRENAMED');
+if ~exist(pathToOutput, 'dir')
+    mkdir(pathToOutput);
 end
 
-% Set path to input folder #2.
-pathToFolder2 = fullfile(getpref(projectName,'BaseDir'),experimentName,'ImageRGBs');
-
-% Set path to output folder #2.
-pathToOutput2 = fullfile(getpref(projectName,'BaseDir'),experimentName,'ImageRGBsRENAMED');
-if ~exist(pathToOutput2, 'dir')
-    mkdir(pathToOutput2);
-end
-
-%% FOLDER #1: Get names of all files in the input folder
+%% Get names of all files in the input folder
 %
 % List .mat files in the folder.
-fileInfo = dir([pathToFolder1 '/*.mat']);
+fileInfo = dir([pathToFolder '/*.mat']);
 
-%% FOLDER #1: Change names of specified files, then save in output folder
+%% Change names of specified files, then save in output folder
 %
 % Check each .mat file in the input folder.
 for ii = 1:length(fileInfo)
@@ -57,7 +48,7 @@ for ii = 1:length(fileInfo)
     % Get the file name.
     fileName = fileInfo(ii).name;
     % Specify the file path.
-    fileToLoad = fullfile(pathToFolder1,fileName);
+    fileToLoad = fullfile(pathToFolder,fileName);
     
     % Break down file name.
     p  = strfind(fileName,'_');
@@ -72,46 +63,7 @@ for ii = 1:length(fileInfo)
         % Specify new file name.
         fileNameNew = sprintf('%s%s%s%s',s1,s2,'_noise2',s4);
         % Specify new file path.
-        fileToSave = fullfile(pathToOutput1,fileNameNew);
- 
-        % Load the 'scene' variable contained in this .mat file.
-        temp = load(fileToLoad,'scene'); scene = temp.scene; clear temp;
-        
-        % Save renamed file in the new output folder.
-        save(fileToSave,'scene');
-        fprintf('Renamed file saved as %s\n', fileToSave);
-    end
-end
-
-%% FOLDER #2: Get names of all files in the input folder
-%
-% List .mat files in the folder.
-fileInfo = dir([pathToFolder2 '/*.mat']);
-
-%% FOLDER #2: Change names of specified files, then save in output folder
-%
-% Check each .mat file in the input folder.
-for ii = 1:length(fileInfo)
-    
-    % Get the file name.
-    fileName = fileInfo(ii).name;
-    % Specify the file path.
-    fileToLoad = fullfile(pathToFolder2,fileName);
-    
-    % Break down file name.
-    p  = strfind(fileName,'_');
-    s1 = fileName(1:p(1)-1);
-    s2 = fileName(p(1):p(2)-1);
-    s4 = fileName(p(3):end);
-    
-    % Check whether to change file name.
-    imageNoiseAmount = str2double(regexp(s4,'[+-]?\d*','Match'));
-    if ismember(imageNoiseAmount,[-30 -27 -24 -21 -18 18 21 24 27 30])
-        
-        % Specify new file name.
-        fileNameNew = sprintf('%s%s%s%s',s1,s2,'_noise2',s4);
-        % Specify new file path.
-        fileToSave = fullfile(pathToOutput2,fileNameNew);
+        fileToSave = fullfile(pathToOutput,fileNameNew);
  
         % Load the 'scene' variable contained in this .mat file.
         temp = load(fileToLoad,'scene'); scene = temp.scene; clear temp;
