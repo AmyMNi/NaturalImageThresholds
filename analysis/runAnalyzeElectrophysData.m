6bc21b05f11cf0320d5b6e4ef131d18b70aec144
AmyMNi <amymni@gmail.com>
2021-10-28 12:24:16 -0400

    minor edits

--- analysis/runAnalyzeElectrophysData.m
+++ analysis/runAnalyzeElectrophysData.m
@@ -28,63 +28,44 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% params.set = 7 (image sets 1-3):
 % 211013-133559_set1.mat :sRGB image set, 2 iterations/stim, size 180
-
 % 211014-153026_set2.mat : RGB image set, 5 iterations/stim, size 130
-
 % 211015-155409_set2.mat : RGB image set, 4 iterations/stim, size 130
 % 211015-160308_set3.mat : RGB image set, 4 iterations/stim, size 130
-
 % 211016-150335_set3.mat : RGB image set, 4 iterations/stim, size 130
 % 211016-152640_set3.mat : RGB image set, 4 iterations/stim, size 130
 % 211016-161735_set3.mat : RGB image set, 3 iterations/stim, size 130
-
 % 211017-155234_set3.mat : RGB image set, 8 iterations/stim, size 140
 % 211017-164400_set3.mat : RGB image set, 3 iterations/stim, size 140
-
 % 211018-145436_set3.mat : RGB image set, 1 iterations/stim, size 140
 % 211018-153731_set3.mat : RGB image set, 1 iterations/stim, size 140
 % 211018-155308_set3.mat : RGB image set, 8 iterations/stim, size 140
-
 % 211019-140217_set3.mat : RGB image set, 1 iterations/stim, size 140
 % 211019-143159_set3.mat : RGB image set, 4 iterations/stim, size 140
-
 % 211020-131825_set3.mat : RGB image set, 6 iterations/stim, size 140
-
 % 211022-170033_set3.mat : RGB image set,<1 iterations/stim, size 90
 % 211022-170332_set3.mat : RGB image set, 4 iterations/stim, size 90
 % 211022-171307_set3.mat : RGB image set, 4 iterations/stim, size 120
 % 211022-172101_set3.mat : RGB image set, 4 iterations/stim, size 150
-
 % 211023-150204_set3.mat : RGB image set, 1 iterations/stim, size 120
 % 211023-150848_set3.mat : RGB image set, 4 iterations/stim, size 120
-
 % 211025-145458_set3.mat : RGB image set, 7 iterations/stim, size 120
-
 % 211026-141738_set3.mat : RGB image set, 6 iterations/stim, size 120
 
+% params.set = 7 (image set 3) & params.set = 9 (image set 5 with universal scale):
+% 211027-160930_set3_set5.mat : RGB image sets, 4 iterations/stim, size 120
 
+%% Record of orthogonality experiment data file names
 
+% params.set = 8 (test image set 4)
+% 211026-143833_set4.mat : RGB image set, 20 iterations/stim, size 120
 
-%{
-ALREADY DOWNLOADED:
-set 4:
-~20 reps ivory_map_211026-143833
+% params.set = 10 (test image set 6 with universal scale)
+% 211027-160722_set6.mat : RGB image set, 20 iterations/stim, size 120
 
-
-
-
-
-control 1:
-set 7 (with universal scaling off) vs set 9 (with universal scaling on)
-~4 reps ivory_map_211027-160930
-
-control 2 (set 10):
-pbrt colour with yellow, cyan, and blacked out bananas
-~20 reps ivory_map_211027-160722
-%}
-
-
+% params.set = 11 (image set 11 with universal scale)
+%
 
 %% Select electrophysiological data sets to analyze
 
@@ -112,7 +93,8 @@ dataNames = { ...
             '211023-150204_set3.mat', ...
             '211023-150848_set3.mat', ...
             '211025-145458_set3.mat', ...
-            '211026-141738_set3.mat'};
+            '211026-141738_set3.mat', ...
+            '211027-160930_set3_set5.mat'};
 %}
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
